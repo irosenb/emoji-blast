193cfed80abff2ef6bf310c6b6504627c9b97ee8
Isaac Rosenberg <irosenb7@gmail.com>
2015-06-17 10:32:22 -0700

    Initial Commit

---
+++ EmojiBlast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiBlast
+//
+//  Created by Isaac Rosenberg on 6/17/15.
+//  Copyright (c) 2015 irosenb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
