43e37e1d9c26a93ed2bc2ddf605140129c420401
Isaac Rosenberg <irosenb7@gmail.com>
2015-06-21 23:38:28 -0700

    Emoji Collection Cell 😹😹😹😹

---
+++ Emoji Blast/EmojiCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiBlast
+//
+//  Created by Isaac Rosenberg on 6/21/15.
+//  Copyright (c) 2015 irosenb. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    var title: String!
+    var text: String!
+    var emojiLabel: UITextView!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        emojiLabel = UITextView(frame: self.bounds)
+        emojiLabel.font = UIFont.systemFontOfSize(100)
+        emojiLabel.backgroundColor = UIColor.clearColor()
+        self.addSubview(emojiLabel)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
